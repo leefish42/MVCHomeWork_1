d2c8838f464e23f6a347df10525bba8a188cda74
李魚濬 <leefish42@hotmail.com>
2017-03-12 22:24:06 +0800

    1.新建專案 2.新增客戶DB

---
+++ MVCHomeWork_1/MVCHomeWork_1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCHomeWork_1.Startup))]
+namespace MVCHomeWork_1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
